2ec66ac51eeeb49d637bdfd2abfe16720d3caeb6
Reshmi Krishna <reshmi9k@gmail.com>
2017-11-30 10:05:18 -0500

    spring one edgware changes

--- service1/src/main/java/io/spring/cloud/sleuth/docs/service1/Service2Client.java
+++ service1/src/main/java/io/spring/cloud/sleuth/docs/service1/Service2Client.java
@@ -5,6 +5,7 @@ import java.lang.invoke.MethodHandles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.sleuth.Span;
 import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.cloud.sleuth.annotation.SpanTag;
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Marcin Grzejszczak
+ * @author Reshmi Krishna
  */
 @Component
 class Service2Client {
@@ -25,8 +27,8 @@ class Service2Client {
 	private final Tracer tracer;
 
 	Service2Client(RestTemplate restTemplate,
-			@Value("${service2.address:localhost:8082}") String serviceAddress,
-			Tracer tracer) {
+				   @Value("${service2.address:localhost:8082}") String serviceAddress,
+				   Tracer tracer) {
 		this.restTemplate = restTemplate;
 		this.serviceAddress = serviceAddress;
 		this.tracer = tracer;
@@ -34,18 +36,23 @@ class Service2Client {
 
 	public String start() throws InterruptedException {
 		log.info("Hello from service1. Setting baggage foo=>bar");
-		String secretBaggage = tracer.getCurrentSpan().getBaggageItem("baggage");
+		Span span = tracer.getCurrentSpan();
+		String secretBaggage = span.getBaggageItem("baggage");
 		log.info("Super secret baggage item for key [baggage] is [{}]", secretBaggage);
 		if (StringUtils.hasText(secretBaggage)) {
-			tracer.getCurrentSpan().logEvent("secret_baggage_received");
+			span.logEvent("secret_baggage_received");
 			tracer.addTag("baggage", secretBaggage);
 		}
-		tracer.getCurrentSpan().setBaggageItem("foo", "bar");
+		String baggageKey = "key";
+		String baggageValue = "foo";
+		span.setBaggageItem(baggageKey, baggageValue);
+		span.logEvent("baggage_set");
+		tracer.addTag(baggageKey, baggageValue);
 		log.info("Hello from service1. Calling service2");
 		String response = restTemplate.getForObject("http://" + serviceAddress + "/foo", String.class);
 		Thread.sleep(100);
 		log.info("Got response from service2 [{}]", response);
-		log.info("Service1: Baggage for [foo] is [" + tracer.getCurrentSpan().getBaggageItem("foo") + "]");
+		log.info("Service1: Baggage for [foo] is [" + span.getBaggageItem("foo") + "]");
 		return response;
 	}
 
